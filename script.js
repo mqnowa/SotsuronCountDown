b7729f7944224cdfa76a4ce6e4ab33a3971d23e0
mqnowa <44436695+mqnowa@users.noreply.github.com>
2025-01-16 22:30:30 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function main() {
+    const BACH_DEAD_LINE = new Date("2025-01-31T16:00:00+09:00").getTime();
+    const MAST_DEAD_LINE = new Date("2025-01-30T16:00:00+09:00").getTime();
+    const BACH_CD_D = document.getElementById("cd-bac-day");
+    const BACH_CD_H = document.getElementById("cd-bac-hour");
+    const BACH_CD_M = document.getElementById("cd-bac-min");
+    const BACH_CD_S = document.getElementById("cd-bac-sec");
+    const MAST_CD_D = document.getElementById("cd-mas-day");
+    const MAST_CD_H = document.getElementById("cd-mas-hour");
+    const MAST_CD_M = document.getElementById("cd-mas-min");
+    const MAST_CD_S = document.getElementById("cd-mas-sec");
+
+    var bach_remain;
+
+    var time_exact_last_refresh;
+    var time_local_last_refresh;
+    var time_local_now;
+
+    document.getElementById("bac-remain").textContent = new Date(BACH_DEAD_LINE)
+    document.getElementById("mas-remain").textContent = new Date(MAST_DEAD_LINE)
+
+    var refresh_time = async () => {
+        fetch("https://elp.emptybox.win/")
+            .then(res => res.json())
+            .then(data => time_exact_last_refresh = data.unix_time_ms);
+        // time_exact_last_refresh = Date.now();
+        time_local_last_refresh = Date.now();
+    }
+
+    refresh_time();
+
+    setInterval(() => {
+        time_exact_now = time_exact_last_refresh + (new Date() - time_local_last_refresh);
+
+        bach_remain = (BACH_DEAD_LINE - time_exact_now) / 1000;
+        BACH_CD_D.textContent = ("0" + Math.floor(bach_remain / 86400)).slice(-2)
+        BACH_CD_H.textContent = ("0" + Math.floor(bach_remain / 3600 % 24)).slice(-2)
+        BACH_CD_M.textContent = ("0" + Math.floor(bach_remain / 60 % 60)).slice(-2)
+        BACH_CD_S.textContent = ("0" + (bach_remain % 60).toFixed(1)).slice(-4)
+        
+        mast_remain = (MAST_DEAD_LINE - time_exact_now) / 1000;
+        MAST_CD_D.textContent = ("0" + Math.floor(mast_remain / 86400)).slice(-2)
+        MAST_CD_H.textContent = ("0" + Math.floor(mast_remain / 3600 % 24)).slice(-2)
+        MAST_CD_M.textContent = ("0" + Math.floor(mast_remain / 60 % 60)).slice(-2)
+        MAST_CD_S.textContent = ("0" + (mast_remain % 60).toFixed(1)).slice(-4)
+    }, 1000 / 20);
+
+    setInterval(() => {
+        refresh_time();
+    }, 300000);
+}
+
+addEventListener("DOMContentLoaded", () => main());
